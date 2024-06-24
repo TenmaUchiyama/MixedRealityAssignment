765d94ef9f700a61d62e6db92eac22071ed7b160
tokudaryousuke <rtokuda.0928@gmail.com>
2024-06-24 14:30:30 +0900

    test

--- Assets/Main/Ryosuke/script/Move.cs
+++ Assets/Main/Ryosuke/script/Move.cs
@@ -8,6 +8,7 @@ public class Move : MonoBehaviour
     public GameObject point;
     //GameObject型を変数charaで宣言します。
     public GameObject chara;
+    public GameObject target;
 
     //コライダーが乗った時の関数
     private void OnTriggerEnter(Collider other)
@@ -15,9 +16,13 @@ public class Move : MonoBehaviour
         //もしゴールオブジェクトのコライダーに接触した時の処理。
         if (other.name == chara.name)
         {
-            //Charaが接触したらpointオブジェクトの位置に移動するよ！
-            chara.transform.position = point.transform.position;
+            //Charaが接触したらpointオブジェクトの位置に移動！
+            //chara.transform.position = point.transform.position;
 
+            Instantiate(target, point.transform.position, Quaternion.identity);
+            Destroy(chara, .1005f);
         }
     }
+
+    
 }
